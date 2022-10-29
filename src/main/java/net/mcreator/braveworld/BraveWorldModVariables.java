421dd4f1a94627abe98584cebb9d8a98191f4a74
Xlebush <unconfigured@null.spigotmc.org>
2022-10-29 15:55:42 +0300

    Начало

---
+++ src/main/java/net/mcreator/braveworld/BraveWorldModVariables.java
@@ -0,0 +1,11 @@
+package net.mcreator.braveworld;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class BraveWorldModVariables {
+	public BraveWorldModVariables(BraveWorldModElements elements) {
+	}
+
+	public static List<Object> s = new ArrayList<>();
+}
